81b94dd9b4a7a172255d29b138893f7bb62d60b5
Beardmaan <39084066+Beardmaan@users.noreply.github.com>
2018-06-05 12:17:44 +0300

    Add files via upload

---
+++ InfixToPostfix1/InfixToPostfix1/Element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfixToPostfix1
+{
+    class Element
+    {
+        public string Content { get; set; }
+        public ElementType Type { get; set; }
+        public int Priority { get; set; }
+    }
+}
---
+++ InfixToPostfix1/InfixToPostfix1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfixToPostfix1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите инфиксное выражение");
+            string line = Console.ReadLine();
+            string result = InfixToPostfixConverter.Convert(line);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
